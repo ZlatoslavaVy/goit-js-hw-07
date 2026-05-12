9147850653abed62d12960164d1b4e20d1b0f3a4
Златослава Вишківська <zlatoslava6b@gmail.com>
2026-05-12 13:46:10 +0300

    do task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,10 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector(".change-color");
+button.addEventListener("click", () => {
+  const spanColor = document.querySelector(".color");
+  spanColor.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = getRandomHexColor();
+});
