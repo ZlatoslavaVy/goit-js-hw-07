0268114e96459263621f7c04069c6ee212fc2d9e
Златослава Вишківська <zlatoslava6b@gmail.com>
2026-05-12 13:17:42 +0300

    do task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,28 @@
 
+const refs = {
+    loginForm: document.querySelector('.login-form'),  
+};
+
+
+refs.loginForm.addEventListener('submit', event => {
+    event.preventDefault();
+    
+    const formData = {
+        [refs.loginForm.elements.email.name]: refs.loginForm.elements.email.value.trim(),
+        [refs.loginForm.elements.password.name]: refs.loginForm.elements.password.value.trim(),
+    };
+
+    const formDataValues = Object.values(formData);
+
+    if (formDataValues.includes('')) {
+        alert('All form fields must be filled in');
+
+        return;
+    }
+
+    console.log(formData);
+
+    refs.loginForm.reset();
+});
+
+
